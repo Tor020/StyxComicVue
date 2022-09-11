ef500ef5c1919a905006be9e8775f03bb1c09377
Tor020 <kopp020@gmail.com>
2022-09-10 21:30:04 -0400

    default ahk commit message

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 // import './assets/main.css'
-import style from './sass/style.scss';
+import style from './sass/styx.scss';
 
 
 const app = createApp(App)
